59299682b074da9a3d2e6c7826c5043820c7c2d1
Stefan Eroteev <eroteev@gmail.com>
2023-08-08 13:45:33 +0300

    Implement deploy script and task

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const lazyImport = async(module: any) => {
+  return await import(module);
+}
+
+task("deploy", "Deploys contract").setAction(async() => {
+  const { main } = await lazyImport("./scripts/deploy.ts");
+  await main();
+})
+
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
 };
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,26 +1,10 @@
 import { ethers } from "hardhat";
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = ethers.parseEther("0.001");
-
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+export async function main() {
+  const library = await ethers.deployContract("Library");
+  await library.waitForDeployment();
+  console.log(`The Library contract is deployed to ${library.target}`);
 }
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
